3aec5a6fef35a86d732fccdc98e036c6efd9749f
iaigner <isabella.aigner@redlink.at>
2026-06-29 12:30:20 +0200

    #420: fix fe bugs for question observations

--- src/utils/setPlaceholderText.ts
+++ src/utils/setPlaceholderText.ts
@@ -1,3 +1,4 @@
-export function varifyPlaceholderText(descriptionVar: string, placeholderString: string): string | undefined {
-  return descriptionVar ? `${descriptionVar.split('.description')[0]}.${placeholderString}` : undefined
+export function varifyPlaceholderText(descriptionVar: string, placeholderString: string): string {
+  const split = descriptionVar.split('.description'); // only now splits when description is part of i18n variable, to ensure correct handling
+  return descriptionVar && split.length > 1 ? `${split[0]}.${placeholderString}` : descriptionVar
 }
